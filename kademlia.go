cf70014f26d6b2d0e7279f968d56fd70f0c0cc27
Michael Rashkovsky <mrashkovsky@gmail.com>
2018-10-30 23:05:16 -0400

    Tweak kxor() and its tests

--- kademlia.go
+++ kademlia.go
@@ -57,8 +57,8 @@ func kxor(m []byte, n []byte) (kbucket_indx int) {
 		o := m[address_section]
 		y := n[address_section]
 		xored = o ^ y
-		fmt.Println("address section", address_section)
-		fmt.Println("xoring", o, "^", y, "=", xored)
+		//fmt.Println("address section", address_section)
+		//fmt.Println("xoring", o, "^", y, "=", xored)
 		if xored > 0 {
 			differs = true
 			break
@@ -66,35 +66,39 @@ func kxor(m []byte, n []byte) (kbucket_indx int) {
 	}
 
 	if !differs {
-		return 1
+		return 0
 	}
 
-	var f uint8 = 0xFF
+	var f uint8 = 1 << 7
+	//fmt.Printf("F (binary): %b\n", f)
 	var place int
-	for place = 0; place < 7; f = f >> 1 {
+	for place = 7; place >= 0; f = f >> 1 {
 		// "Sieve" thing to get the bit # of first differing bit
 		//fmt.Println("xored,f", xored, f)
-		if xored > f {
-			place -= 1 // Went too far, shift back one
+		if xored >= f {
 			break
 		}
-		place += 1
+		place -= 1
 	}
 
-	fmt.Println("address_section, place", address_section, place)
+	//fmt.Println("address_section, place", address_section, place)
 	kbucket_indx = address_section*8 + place
 	return kbucket_indx
 }
 
 func tests() {
+	var l []byte = []byte{0x0, 1, 3, 4}
 	var m []byte = []byte{0x1, 1, 3, 4}
 	var n []byte = []byte{0x2, 1, 3, 4}
-	// var o []byte = []byte{0x3, 1, 3, 4}
-	// var p []byte = []byte{0x4, 1, 3, 4}
-	// fmt.Println("expect one", m, m, kxor(m, m))
+	var o []byte = []byte{0x3, 1, 3, 4}
+	var p []byte = []byte{0x4, 1, 3, 4}
+	var q []byte = []byte{0x5, 1, 3, 4}
+	fmt.Println("expect zero", l, m, kxor(l, m))
+	fmt.Println("expect zero", m, m, kxor(m, m))
 	fmt.Println("expect one", m, n, kxor(m, n))
-	// fmt.Println("expect two", n, o, kxor(n, o))
-	// fmt.Println("expect two", o, p, kxor(o, p))
+	fmt.Println("expect one", n, o, kxor(n, o))
+	fmt.Println("expect two", o, p, kxor(o, p))
+	fmt.Println("expect zero", p, q, kxor(p, q))
 	// fmt.Println("__TEST KXOR__:", "m: ", m, "n: ", n, kxor(m, n))
 	// fmt.Println("__TEST KXOR__:", "o: ", o, "p: ", p, kxor(o, p))
 }
