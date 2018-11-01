6800fad8325a5d3b4a314445c5d8f45731d4a254
Michael Rashkovsky <mrashkovsky@gmail.com>
2018-10-31 20:22:36 -0400

    Handle inserts into kbuckets

--- kademlia.go
+++ kademlia.go
@@ -17,9 +17,11 @@ var N int = 32
 var K int = 20
 var A int = 3
 
+type address []byte
+
 type node struct {
-	nodeid   []byte
-	kbuckets [][][]byte
+	nodeid   address
+	kbuckets [][]address
 }
 
 func (n node) new() node {
@@ -29,17 +31,29 @@ func (n node) new() node {
 	b = byte(rand.Intn(math.MaxUint8))
 	c = byte(rand.Intn(math.MaxUint8))
 	d = byte(rand.Intn(math.MaxUint8))
-	n.nodeid = []byte{a, b, c, d}
-	n.kbuckets = make([][][]byte, N)
+	n.nodeid = address{a, b, c, d}
+	n.kbuckets = make([][]address, N)
 	return n
 }
 
-func (self node) kbucket_insert(neighbor_nodeid []byte) bool {
-	kbucket := self.kbuckets[kxor(self.nodeid, neighbor_nodeid)]
-	fmt.Println(len(kbucket), cap(kbucket))
-	for i := 0; i < len(kbucket); i++ {
-		fmt.Println(kbucket[i])
+func (self node) kbucket_insert(neighbor_nodeid address) bool {
+	kdistance := kxor(self.nodeid, neighbor_nodeid)
+	kbucket := self.kbuckets[kdistance]
+
+	if len(kbucket) <= 20 {
+		self.kbuckets[kdistance] = append(kbucket, neighbor_nodeid)
+		kbucket = self.kbuckets[kdistance]
+		return true
+	} else {
+		// Out of space, handle this as an LRU & keep a cache of substitute nodes ready
+		return false
 	}
+}
+
+func (self node) find_node(target_nodeid address) bool {
+	distance_to_target := kxor(self.nodeid, target_nodeid)
+	//foo []address := self.get_nearest_to(target_nodeid)
+	fmt.Println(distance_to_target)
 	return true
 }
 
@@ -54,14 +68,14 @@ func main() {
 
 	var m node = neighborhood[0]
 	var n node = neighborhood[1]
-	//fmt.Println("m,n,kxor", m, n, kxor(m.nodeid, n.nodeid))
 	// Bootstrap m
 	m.kbucket_insert(n.nodeid)
+	m.find_node(m.nodeid) // have m search for itself
 }
 
 // kxor returns p, the place of the binary digit where their nodeids differ
 // node m files node n into place p of its kbucket array
-func kxor(m []byte, n []byte) (kbucket_indx int) {
+func kxor(m address, n address) (kbucket_indx int) {
 	var address_section int
 	var xored uint8
 	var differs = false
@@ -69,8 +83,6 @@ func kxor(m []byte, n []byte) (kbucket_indx int) {
 		o := m[address_section]
 		y := n[address_section]
 		xored = o ^ y
-		//fmt.Println("address section", address_section)
-		//fmt.Println("xoring", o, "^", y, "=", xored)
 		if xored > 0 {
 			differs = true
 			break
@@ -82,35 +94,30 @@ func kxor(m []byte, n []byte) (kbucket_indx int) {
 	}
 
 	var f uint8 = 1 << 7
-	//fmt.Printf("F (binary): %b\n", f)
 	var place int
 	for place = 7; place >= 0; f = f >> 1 {
 		// "Sieve" thing to get the bit # of first differing bit
-		//fmt.Println("xored,f", xored, f)
 		if xored >= f {
 			break
 		}
 		place -= 1
 	}
 
-	//fmt.Println("address_section, place", address_section, place)
 	kbucket_indx = address_section*8 + place
 	return kbucket_indx
 }
 
 func tests() {
-	var l []byte = []byte{0x0, 1, 3, 4}
-	var m []byte = []byte{0x1, 1, 3, 4}
-	var n []byte = []byte{0x2, 1, 3, 4}
-	var o []byte = []byte{0x3, 1, 3, 4}
-	var p []byte = []byte{0x4, 1, 3, 4}
-	var q []byte = []byte{0x5, 1, 3, 4}
+	var l address = address{0x0, 1, 3, 4}
+	var m address = address{0x1, 1, 3, 4}
+	var n address = address{0x2, 1, 3, 4}
+	var o address = address{0x3, 1, 3, 4}
+	var p address = address{0x4, 1, 3, 4}
+	var q address = address{0x5, 1, 3, 4}
 	fmt.Println("expect zero", l, m, kxor(l, m))
 	fmt.Println("expect zero", m, m, kxor(m, m))
 	fmt.Println("expect one", m, n, kxor(m, n))
 	fmt.Println("expect one", n, o, kxor(n, o))
 	fmt.Println("expect two", o, p, kxor(o, p))
 	fmt.Println("expect zero", p, q, kxor(p, q))
-	// fmt.Println("__TEST KXOR__:", "m: ", m, "n: ", n, kxor(m, n))
-	// fmt.Println("__TEST KXOR__:", "o: ", o, "p: ", p, kxor(o, p))
 }
